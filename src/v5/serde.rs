1cb677a1665ba60e4323b1863b03e62936c6cd55
Yurii Musolov <yurii.musolov@gmail.com>
2025-06-13 18:02:12 +0200

    chore: add function deserialize_str

--- src/v5/serde.rs
+++ src/v5/serde.rs
@@ -1,10 +1,19 @@
-use serde::Deserialize;
-
 pub fn invalid_as_none<'de, D, T>(deserializer: D) -> Result<Option<T>, D::Error>
 where
     D: serde::de::Deserializer<'de>,
-    T: Deserialize<'de>,
+    T: serde::Deserialize<'de>,
 {
-    let res = Deserialize::deserialize(deserializer).unwrap_or(None);
+    let res = serde::Deserialize::deserialize(deserializer).unwrap_or(None);
     Ok(res)
+}
+
+pub fn deserialize_str<'de, T>(
+    json: &'de str,
+) -> Result<T, serde_path_to_error::Error<serde_json::Error>>
+where
+    T: serde::de::Deserialize<'de>,
+{
+    let deserializer = &mut serde_json::Deserializer::from_str(json);
+
+    serde_path_to_error::deserialize(deserializer)
 }
